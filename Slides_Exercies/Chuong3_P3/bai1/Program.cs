ccaad8aff89cf01b6f9aebccf56e6bef6f68b894
luhoangve24 <lehoang242002@gmail.com>
2022-03-05 17:29:50 +0700

    Update Bai tap

---
+++ Slides_Exercies/Chuong3_P3/bai1/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace bai1
+{
+    class SinhVien
+    {
+        string _HoTen;
+        DateTime _NgaySinh;
+        double _LT, _CSDL, _WEB, _DTB;
+
+        public SinhVien(string name, DateTime ns,
+                    double LT, double CSDL, double WEB)
+        {
+            _HoTen = name;
+            _NgaySinh = ns;
+            _LT = LT;
+            _CSDL = CSDL;
+            _WEB = WEB;
+            _DTB = (_LT + _CSDL + _WEB)/3;
+        }
+
+        public SinhVien()
+        {
+            _HoTen = "";
+            _LT = 0;
+            _CSDL = 0;
+            _WEB = 0;
+            _DTB = 0;
+        }
+
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap ho va ten: ");
+            _HoTen = Console.ReadLine();
+            Console.WriteLine("Nhap ngay sinh: ");
+            _NgaySinh = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Nhap diem Lap trinh: ");
+            _LT = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap diem CSDL: ");
+            _CSDL = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap diem Lap trinh Web: ");
+            _WEB = Convert.ToDouble(Console.ReadLine());
+            _DTB = (_LT + _CSDL + _WEB)/3;
+        }
+
+        public double TrungBinh
+        {
+            get
+            {
+                return _DTB;
+            }
+        }
+
+        public double LapTrinh
+        {
+            get
+            {
+                return _LT;
+            }
+        }
+
+        public double Coso
+        {
+            get
+            {
+                return _CSDL;
+            }
+        }
+
+        public double ltWeb
+        {
+            get
+            {
+                return _WEB;
+            }
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", _HoTen, _NgaySinh.ToShortDateString(), _LT, _CSDL, _WEB, _DTB);
+        }
+    }
+
+    class DanhSachSV
+    {
+        int _SL; // So luong
+        SinhVien[] _DS; // Danh sach
+
+        public DanhSachSV(int n)
+        {
+            _SL = n;
+            _DS = new SinhVien[_SL];
+        }
+
+        public DanhSachSV()
+        {
+            _SL = 0;
+            Console.WriteLine("Chua nhap so luong, moi ban nhap");
+            _SL = Convert.ToInt32(Console.ReadLine());
+            _DS = new SinhVien[_SL];
+        }
+
+        public void Nhap()
+        {
+            for (int i = 0; i < _SL; i++)
+            {
+                Console.Clear();
+                _DS[i] = new SinhVien();
+                _DS[i].Nhap();               
+            }
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("--Ho ten--Ngay sinh--Lap trinh--CSDL--Web--DTB");
+            foreach (SinhVien item in _DS)
+            {
+                item.Xuat();
+            }
+        }
+
+        public void KhoaLuan_ChuyenDe()
+        {
+            Console.WriteLine("Sinh vien lam khoa luan: ");
+            for (int i = 0; i < _SL; i++)
+            {
+                if(_DS[i].TrungBinh>=8 && _DS[i].Coso>=5 && _DS[i].LapTrinh>=5 && _DS[i].ltWeb>=5)
+                {
+                    _DS[i].Xuat();
+                }
+            }
+
+            Console.WriteLine("Sinh vien lam chuyen de: ");
+            for (int i = 0; i < _SL; i++)
+            {
+                if(_DS[i].TrungBinh<8 && _DS[i].Coso>=5 && _DS[i].LapTrinh>=5 && _DS[i].ltWeb>=5)
+                {
+                    _DS[i].Xuat();
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DanhSachSV Lop10A = new DanhSachSV(2);
+            Lop10A.Nhap();
+            Lop10A.Xuat();
+            Lop10A.KhoaLuan_ChuyenDe();
+        }
+    }
+}
---
+++ Slides_Exercies/Chuong3_P3/bai2/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+namespace bai2
+{
+    abstract class Xe
+    {
+        abstract public void Nhap();
+        abstract public void Xuat();
+        abstract public double TinhTien(double sogio);
+    }
+
+    class XeTai : Xe
+    {
+        string _Bien;
+        double _TrongTai;
+        public override void Nhap()
+        {
+            Console.WriteLine("Nhap bien so: ");
+            _Bien = Console.ReadLine();
+            Console.WriteLine("Nhap trong tai (kg): ");
+            _TrongTai = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override void Xuat()
+        {
+            Console.WriteLine("\t - Bien so {0}", _Bien);
+            Console.WriteLine("\t - Trong tai {0} kg", _TrongTai);
+        }
+
+        public override double TinhTien(double sogio)
+        {
+            if (sogio<=1)
+            {
+                return 220000;
+            }
+            else
+            {
+                return 220000 + (sogio - 1)*85000;
+            }
+        }
+
+    }
+
+    
+    class XeKhach : Xe
+    {
+        string _Bien;
+        int _SoCho;
+        public override void Nhap()
+        {
+            Console.WriteLine("Nhap bien so: ");
+            _Bien = Console.ReadLine();
+
+            Console.WriteLine("So cho ngoi: ");
+            _SoCho = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override void Xuat()
+        {
+            Console.WriteLine("\t- Bien so {0}", _Bien);
+            Console.WriteLine("\t- So cho {0}", _SoCho);
+        }
+
+        public override double TinhTien(double sogio)
+        {
+            if (sogio<=1)
+            {
+                return 250000;
+            }
+            else
+            {
+                return 250000 + (sogio - 1)*70000;
+            }
+        }
+
+    }
+
+    
+    class Khach
+    {
+        string _HoTen;
+        string _TenXe;
+        Xe _LoaiXe;
+        double _SoGio;
+
+        public Khach(string name, string opt, double time)
+        {
+            _HoTen = name;
+            switch(opt)
+            {
+                case "XK":
+                {
+                    _TenXe = "Xe Khach";
+                    _LoaiXe = new XeKhach();
+                    break;
+                }
+                case "XT":
+                {
+                    _TenXe = "Xe Tai";
+                    _LoaiXe = new XeTai();
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Khong ton tai ma xe");
+                    break;
+                }
+            }
+            _SoGio = time;
+        }
+
+        public Khach()
+        {
+            _TenXe = "";
+            _HoTen = "";
+            _SoGio = 0;
+        }
+
+        private void LuaChon(ref Xe oto)
+        {
+            int opts;
+            Console.WriteLine("Xe: \n1.Tai \n2.Khach");
+            opts = Convert.ToInt32(Console.ReadLine());
+            switch(opts)
+            {
+                case 1:
+                {
+                    _TenXe = "Xe Tai";
+                    oto = new XeTai();
+                    oto.Nhap();
+                    break;
+                }
+
+                case 2:
+                {
+                    _TenXe = "Xe Khach";
+                    oto = new XeKhach();
+                    oto.Nhap();
+                    break;
+                }
+
+                default:
+                {
+                    Console.WriteLine("Loi, khong co xe nay");
+                    break;
+                }
+            }
+        }
+
+        public void Nhap()
+        {
+            Console.Clear();
+            Console.WriteLine("Nhap ho ten: ");
+            _HoTen = Console.ReadLine();
+            LuaChon(ref _LoaiXe);
+            Console.WriteLine("Nhap so gio thue: ");
+            _SoGio = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("-- {0} -- {1} -- {2} -- {3:C} --", _HoTen, _SoGio, _TenXe, _LoaiXe.TinhTien(_SoGio));
+            _LoaiXe.Xuat();
+
+        }
+
+        public double TinhTien()
+        {
+            return _LoaiXe.TinhTien(_SoGio);
+        }
+    }
+
+    
+    class QuanLy
+    {
+        int _SL; // So luong
+        Khach[] _DS; // Danh sach
+
+        public QuanLy(int n)
+        {
+            _SL = n;
+            _DS = new Khach[_SL];
+        }
+
+        public QuanLy()
+        {
+            _SL = 0;
+        }
+
+        public void Nhap()
+        {
+            if (_SL == 0)
+            {
+                Console.WriteLine("Nhap so luong khach: ");
+                _SL = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < _SL; i++)
+            {
+                _DS[i] = new Khach();
+                _DS[i].Nhap();
+            }
+        }
+
+        public void Xuat()
+        {
+            Console.WriteLine("-- Ho ten -- So gio thue -- Loai xe thue -- Thanh tien --");
+            for (int i = 0; i < _SL; i++)
+            {
+                _DS[i].Xuat();
+            }
+        }
+
+        public void TongTien()
+        {
+            double TotalRevenue = 0;
+            for (int i = 0; i < _SL; i++)
+            {
+                TotalRevenue += _DS[i].TinhTien();
+            }
+
+            Console.WriteLine("Tong doanh thu: {0:C}", TotalRevenue);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuanLy CTY = new QuanLy(2);
+            CTY.Nhap();
+            CTY.Xuat();
+            CTY.TongTien();
+        }
+    }
+}
