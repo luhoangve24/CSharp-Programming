3e78b04809a3d6b8e740250f94b694a2330c2004
Le Hoang Vu <92226066+luhoangve24@users.noreply.github.com>
2022-04-16 10:07:55 +0700

    BaiTapLon
    Library Management

---
+++ BTL_BaoCao/Quan_ly_thue_sach/Quan_ly_thue_sach/Quan_ly_thue_sach/Forms/FormLogin.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Quan_ly_thue_sach.Forms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnDN = new System.Windows.Forms.Button();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnDong = new System.Windows.Forms.Button();
+            this.lblLoi = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDN
+            // 
+            this.btnDN.Location = new System.Drawing.Point(99, 136);
+            this.btnDN.Name = "btnDN";
+            this.btnDN.Size = new System.Drawing.Size(125, 34);
+            this.btnDN.TabIndex = 0;
+            this.btnDN.Text = "Đăng nhập";
+            this.btnDN.UseVisualStyleBackColor = true;
+            this.btnDN.Click += new System.EventHandler(this.btnDN_Click);
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(188, 55);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(190, 22);
+            this.txtTK.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tài khoản";
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(188, 99);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(190, 22);
+            this.txtMK.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(0, 199);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 64);
+            this.panel1.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(65, 10);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(70, 17);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Facebook";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(285, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Bây giờ là";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTime.Location = new System.Drawing.Point(285, 38);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(12, 17);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Hỗ trợ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(12, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Đường dây nóng: 091878xxxx";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(253, 136);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(125, 34);
+            this.btnDong.TabIndex = 0;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // lblLoi
+            // 
+            this.lblLoi.AutoSize = true;
+            this.lblLoi.ForeColor = System.Drawing.Color.Red;
+            this.lblLoi.Location = new System.Drawing.Point(185, 20);
+            this.lblLoi.Name = "lblLoi";
+            this.lblLoi.Size = new System.Drawing.Size(46, 17);
+            this.lblLoi.TabIndex = 5;
+            this.lblLoi.Text = "label6";
+            this.lblLoi.Visible = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 263);
+            this.Controls.Add(this.lblLoi);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnDN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDN;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Label lblLoi;
+    }
+}
---
+++ BTL_BaoCao/Quan_ly_thue_sach/Quan_ly_thue_sach/Quan_ly_thue_sach/Forms/FormLogin.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quan_ly_thue_sach.Classes;
+
+namespace Quan_ly_thue_sach.Forms
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            timer1.Start();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void btnDN_Click(object sender, EventArgs e)
+        {
+            string userName, userPassword;
+
+            if (txtTK.Text.Trim().Length == 0 || txtMK.Text.Trim().Length == 0)
+            {
+                lblLoi.Text = "**Chưa nhập đầy đủ thông tin!**";
+                lblLoi.Visible = true;
+                return;
+            } 
+
+            userName = txtTK.Text;
+            userPassword = txtMK.Text;
+            if (Funtions.Login(userName, userPassword))
+            {
+                MessageBox.Show("Chào Admin, chúc một ngày làm việc vui vẻ :) !");
+                Forms.frmMain f = new frmMain();
+                this.Hide();
+                f.ShowDialog();
+                Funtions.NgatKetNoi();
+            }
+            else
+            {
+                MessageBox.Show("Sai rồi, mời bạn nhập lại");
+                return;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/luhoangve/");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
